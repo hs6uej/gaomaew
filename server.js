89c530108ee865ff59b68e81f1ef589dc40dda60
hs6uej <hs6uej@gmail.com>
2026-04-19 02:47:11 +0700

    edit dockerfile4

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/api/highscores', (req, res) => {
 });
 
 // Fallback to React index.html
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
